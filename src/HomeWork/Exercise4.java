cec48ca0ccdffaddfd5e58e497b20c70187f5321
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-10-26 18:49:58 +0300

    Update Exercise4.java

--- src/HomeWork/Exercise4.java
+++ src/HomeWork/Exercise4.java
@@ -1,16 +1,23 @@
 package HomeWork;
 
-
 public class Exercise4 {
-
     public static void main(String[] args) {
-        int a = 15;
-        int b = 5;
+        int j = 0, a = 15,  b = 5;
         for (int i = 1; i <= 10; i++) {
-            if((a + b) % i == 0) System.out.println((a + b) + "/"  + i + "  Делится без остатка ");
-            switch (1) {
-                case 1:
+            if ((a + b) % i == 0) {
+                j = 1;
+            } else {
+                j = 2;
+            }
+            switch (j) {
+                case 1: {
+                    System.out.println((a + b) + "/" + i + "  Делится без остатка ");
                     break;
+                }
+                case 2: {
+                    System.out.println(" Делится  на " + i + " с остатком  "  + (a + b) % i);
+                    break;
+                }
             }
         }
     }
