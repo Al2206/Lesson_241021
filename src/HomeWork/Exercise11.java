21289c68637f2afd400691e1a0c6e90a56d14e4a
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-10-27 16:58:59 +0300

    Update Exercise11.java

--- src/HomeWork/Exercise11.java
+++ src/HomeWork/Exercise11.java
@@ -1,34 +1,21 @@
 package HomeWork;
 
 import java.util.Scanner;
-public class Exercise10 {
+public class Exercise11 {
     public static void main(String[] args) {
-        int x, i;
+        double x;
+        double result1;
+        double result2;
         Scanner in = new Scanner(System.in);
         System.out.println("Введите число:  ");
-        System.out.println("Введите номер оператора  от 1 до 3:  ");
         x = in.nextInt();
-        i = in.nextInt();
-        if  (i == 1) {
-          x /= x ;
-        } else  if (i == 2) {
-            x *= x;
-        } else if (i == 3) {
-            x = x * x - x;
-        } else  if ( i > 3) {
-            System.out.println("Введено неверное значение ");
+        if (x % 2 == 0) {
+            result2 = x * x;
+            System.out.println(result2);
         }
-        switch (i) {
-            case 1:
-                System.out.println(x);
-                break;
-            case 2:
-                System.out.println(x);
-                break;
-            case 3:
-                System.out.println(x);
-                break;
-            default:
+            else {
+            result1 = (x + (x % 2));
+            System.out.println(result1);
         }
     }
 }
